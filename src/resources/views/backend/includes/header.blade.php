a28caea141be53bc47c6ab9afeca42ce632bc37f
Alex <alex@10168.com>
2019-09-17 14:32:19 +1200

    Fix: top-logo url

--- src/resources/views/backend/includes/header.blade.php
+++ src/resources/views/backend/includes/header.blade.php
@@ -3,30 +3,31 @@
         <div class="col-auto menu-logo">
             <h2>
                 <a href="{{ route('LaravelCmsAdmin.index') }}" title="{{ $helper->t('dashboard')}}"><img
-                        class="img-fluid top-logo" src="{{$helper->assetUrl('images/top-logo2.png', false, true) }}" style="height:80px;"
-                        alt="Laravel CMS" /></a>
+                        class="img-fluid top-logo" src="{{$helper->assetUrl('images/top-logo.png', false, true) }}"
+                        style="height:80px;" alt="Laravel CMS" /></a>
             </h2>
         </div>
         <div class="col text-right pt-3 menu-links">
             @php
             $menu_links = $helper->s('category.admin_menu_links');
             if ( is_array($menu_links)){
-                foreach($menu_links as $link){
-                    if ( is_array($link)) {
-                        if ( $link['style'] == 'dropdown'){
-                            echo '<div class="btn-group">
-                                ' . $link['button'] . '
-                                    <div class="dropdown-menu">';
-                                foreach( $link['items'] as $item ){
-                                    echo $item;
-                                }
-                            echo '</div></div>';
-                        }
+            foreach($menu_links as $link){
+            if ( is_array($link)) {
+            if ( $link['style'] == 'dropdown'){
+            echo '<div class="btn-group">
+                ' . $link['button'] . '
+                <div class="dropdown-menu">';
+                    foreach( $link['items'] as $item ){
+                    echo $item;
                     }
-                    else {
-                        echo $link;
-                    }
-                }
+                    echo '</div>
+            </div>';
+            }
+            }
+            else {
+            echo $link;
+            }
+            }
             }
             @endphp
 
