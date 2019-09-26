81a9bef3877a7f507324a4283e7425df4767c34d
Alex <alex@10168.com>
2019-09-27 09:36:22 +1200

    Move submitInquiryForm() to bottom.js

--- src/resources/views/plugins/page-tab-inquiry-form/frontend-form-001.blade.php
+++ src/resources/views/plugins/page-tab-inquiry-form/frontend-form-001.blade.php
@@ -12,55 +12,8 @@
 
 {!! $gg_recaptcha !!}
 
-<!-- Start of the laravel-cms-inquiry-form script -->
-<script>
-    $("#laravel-cms-inquiry-form").submit(function(event){
-    event.preventDefault();
-    if( typeof(grecaptcha) != 'undefined'){
-        var response = grecaptcha.getResponse();
-        if(response.length == 0) {
-            //alert('Google recaptcha not ticked, no ajax');
-            return false;
-        }
-    }
+{{--
+    A javascript function submitInquiryForm() in the bottom.js will be invoked when the page is ready
 
-    $('#laravel-cms-inquiry-form button[type="submit"]').attr("disabled", "disabled").append('<i class="fas fa-spinner fa-spin ml-2"></i>');
-
-    $.ajax({
-        url : $(this).attr("action"),
-        type: $(this).attr("method"),
-        data : new FormData(this),
-		contentType: false,
-		cache: false,
-        processData:false,
-        dataType: 'json',
-        success: function (data) {
-            console.log('Submission was successful.');
-            //console.log(data);
-            if ( data.success ){
-                $("#laravel-cms-inquiry-form .form-group").fadeOut('slow');
-                $("#laravel-cms-inquiry-form-results").html(data.success_content);
-
-            } else {
-                $('#laravel-cms-inquiry-form .error_message').html('Error: ' + data.error_message);
-
-                $('#laravel-cms-inquiry-form button[type="submit"]').removeAttr("disabled");
-                $('#laravel-cms-inquiry-form button[type="submit"] i.fa-spinner').remove();
-            }
-
-        },
-        error: function (data) {
-            $('#laravel-cms-inquiry-form .error_message').html('Error: ' + data.responseJSON.message);
-            $('#laravel-cms-inquiry-form button[type="submit"]').removeAttr("disabled");
-            $('#laravel-cms-inquiry-form button[type="submit"] i.fa-spinner').remove();
-
-            console.log('laravel-cms-inquiry-form : An error occurred.');
-            console.log(data);
-        },
-    }).done(function(data){ //
-        console.log('laravel-cms-inquiry-form submitted');
-        //console.log(data);
-    });
-});
-</script>
-<!-- End of the laravel-cms-inquiry-form script -->
+    It's better to put the javascript here when develop your own plugin as the frontend bottom.js file are not controllable
+--}}
